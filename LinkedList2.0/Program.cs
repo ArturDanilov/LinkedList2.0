3f3aefcd2004ccaaea6644c98977395661deb52b
Artur Danilov <artur.danilov@adesso.de>
2023-02-09 16:00:18 +0100

    Refactoring, bugs fixed

--- LinkedList2.0/Program.cs
+++ LinkedList2.0/Program.cs
@@ -6,8 +6,7 @@
         {
             var list = new Model.LinkedList<int>(); //Model?
 
-
-            Console.WriteLine();
+            Console.WriteLine("Add elements 1,2,3,4,5,6,7,8,9,10 to the list:");
 
             list.Add(1);
             list.Add(2);
@@ -15,12 +14,18 @@
             list.Add(4);
             list.Add(5);
             list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(9);
+            list.Add(10);
 
             foreach (var item in list)
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Delete elements 3,1,7 from the list:");
 
             list.Delete(3);
             list.Delete(1);
@@ -30,7 +35,9 @@
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Add element 7 to the head:");
 
             list.AppendHead(7);
 
@@ -38,7 +45,9 @@
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Add element 8 after element 4:");
 
             list.InsertAfter(4, 8);
 
@@ -46,7 +55,9 @@
             {
                 Console.Write(item + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("\nThe end!");
 
             Console.ReadLine();
         }
